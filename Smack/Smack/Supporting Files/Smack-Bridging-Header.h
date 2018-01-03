750253f1ae397d8a923573c14905d384aba0ce39
Octavian <octavian@192-168-0-105.rdsnet.ro>
2018-01-03 16:35:03 +0200

    End of lesson 1

---
+++ Smack/Smack/Supporting Files/Smack-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//allows to seamlessly work swift code and objective-C code in the same project
+
+#import "SWRevealViewController.h"
