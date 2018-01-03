7d47d2bafac84c161b0e6883f73cbba1742adfc9
Octavian <octavian@192-168-0-105.rdsnet.ro>
2018-01-03 21:56:40 +0200

    Lesson 9

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -8,7 +8,22 @@
 
 import Foundation
 
+//typealias is simply renaming a type
+//typealias Octavian = String
+//let name: Octavian = "Octavian"
+//(_ Success: Bool) -> () - closure, first class function that can be passed around in code
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://octaviansmack.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register/"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
