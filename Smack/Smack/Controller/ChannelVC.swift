7d40aabd1ff00d771ea94ebcd1529fa69d98ec1c
Octavian <octavian@192-168-0-105.rdsnet.ro>
2018-01-07 01:21:59 +0200

    Lesson 17

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -33,6 +33,14 @@ class ChannelVC: UIViewController {
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        if AuthService.instance.isLoggedIn {
+            //Show profile page
+            //XIB
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+        } else {
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        }
     }
 }
