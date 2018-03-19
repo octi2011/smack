38cd1db920d403eec4e918821a16c7947d735841
Duminica Octavian <duminicaoctavian@Duminicas-MacBook-Pro.local>
2018-03-19 22:36:26 +0200

    fixed the server

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -15,7 +15,7 @@ import Foundation
 typealias CompletionHandler = (_ Success: Bool) -> ()
 
 // URL Constants
-let BASE_URL = "https://octaviansmack.herokuapp.com/v1/"
+let BASE_URL = "https://octaviansmacky.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
